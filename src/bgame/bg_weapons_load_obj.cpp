9f5b5a2956829e4023502abd2136ac14aa8d2b17
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2025-10-08 00:09:14 +0800

    load proper defaultweapon in SP

--- src/bgame/bg_weapons_load_obj.cpp
+++ src/bgame/bg_weapons_load_obj.cpp
@@ -1286,7 +1286,11 @@ WeaponDef *__cdecl BG_LoadWeaponDefInternal(const char *one, const char *two)
                         BG_ParseWeaponDefSpecificFieldType,
                         SetConfigString2))
                     {
+#ifdef KISAK_MP
                         if (I_stricmp(two, "defaultweapon_mp"))
+#elif KISAK_SP
+                        if (I_stricmp(two, "defaultweapon"))
+#endif
                         {
                             if (!weapDef->viewLastShotEjectEffect)
                                 weapDef->viewLastShotEjectEffect = weapDef->viewShellEjectEffect;
@@ -1380,10 +1384,18 @@ WeaponDef *__cdecl BG_LoadWeaponDef_LoadObj(const char *name)
 
     if (!*name)
         return 0;
+#ifdef KISAK_MP
     weapDef = BG_LoadWeaponDefInternal("mp", name);
+#elif KISAK_SP
+    weapDef = BG_LoadWeaponDefInternal("sp", name);
+#endif
     if (weapDef)
         return weapDef;
+#ifdef KISAK_MP
     weapDefa = BG_LoadWeaponDefInternal("mp", "defaultweapon_mp");
+#elif KISAK_SP
+    weapDefa = BG_LoadWeaponDefInternal("sp", "defaultweapon");
+#endif
     if (!weapDefa)
         Com_Error(ERR_DROP, "BG_LoadWeaponDef: Could not find default weapon");
     SetConfigString((char **)weapDefa, name);
